63aa8af67b04af70ee235be0e716df654c053179
Cooper Doten <cooperdoten@gmail.com>
2021-03-30 17:37:36 -0500

    pictures added, animation added

---
+++ src/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import SpringAnimation from './SpringAnimation';
+function Homepage() {
+  return (
+    <div className="Homepage">
+     <SpringAnimation />
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ src/SpringAnimation.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './App.css';
+
+export default function SpringAnimation() {
+ 
+    return (<div className="floating"> Welcom to My Page</div>)
+}
